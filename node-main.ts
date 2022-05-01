23de13b0102aeffe549923538aeedec1f5a0cc1a
cjkkkk <shanbinke@gmail.com>
2022-05-01 02:22:45 -0700

    update cli import object

--- node-main.ts
+++ node-main.ts
@@ -14,22 +14,26 @@ console.log(wasmSource);
 const importObject = {
     imports: {
         print_num: (arg : any) => {
+            console.log(arg)
             importObject.output += arg;
             importObject.output += "\n";
             return arg;
         },
         print_bool: (arg : any) => {
-            if(arg === 0) { 
+            if(arg === 0) {
+                console.log("False")
                 importObject.output += "False";
                 importObject.output += "\n";
             }
-            else { 
+            else {
+                console.log("True")
                 importObject.output += "True";
                 importObject.output += "\n";
             }
             return arg;
         },
         print_none: (arg: any) => {
+            console.log(arg)
             importObject.output += "None";
             importObject.output += "\n";
             return arg;
